031cb7086b08e1a2eb902e3974d9710768cd1dce
WhateverCodes <saachi.aneja@outlook.com>
2026-08-11 17:21:27 +0530

    Add: grumpy-bookstore-owner - submission-5

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        pos = 0
+        ans = 0
+        l = len(customers)
+        while pos < l :
+            if grumpy[pos] == 0 :
+                ans += customers[pos]
+                customers[pos] = 0
+            pos += 1
+        top = 0
+        c = sum(customers[0:minutes])
+        for i in range(l-minutes+1) :
+            top = max(top, c)
+            c -= customers[i]
+            if i+minutes < l : c += customers[i+minutes]
+        return ans+top
