5248bac8c6a27f9e58c9f0676221a75caa014bf1
WhateverCodes <saachi.aneja@outlook.com>
2026-08-26 07:51:39 +0530

    Add: buy-two-chocolates - submission-2

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-2.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        pay = prices[0]+prices[1]
+        if pay > money : return money
+        return money-pay
