c5375f395ecd54f4588306dd785f94453d248db6
WhateverCodes <saachi.aneja@outlook.com>
2026-09-18 13:02:53 +0530

    Add: sql-warehouse-manager - submission-0

---
+++ SQL/sql-warehouse-manager/submission-0.sql
@@ -0,0 +1,7 @@
+-- Write your query below
+select w.name as warehouse_name, sum(w.units*p.width*p.length*p.height) as volume
+from warehouse w
+left join
+products p
+using (product_id)
+group by warehouse_name
